451790e16f7a68e167097065da208c210ba2e417
akhilkumarmallineni-lgtm <akhilkumarmallineni@gmail.com>
2026-07-04 12:25:12 +0530

    Update zone_id and domain_name variables

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -7,11 +7,11 @@ variable "environment" {
 }
 
 variable "zone_id" {
-    default = "Z07086101C1CVP7AT2UK4"
+    default = "Z1004002PSP33W9WXUA4"
 }  
 
 variable "domain_name" {
-    default = "daws90s.shop"
+    default = "akhilkumarshop.online"
 }
 
 variable "mysql_root_password" {
