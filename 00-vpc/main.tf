dd06456413891d5154ea286cbb39f3581e8590ee
akhilkumarmallineni-lgtm <akhilkumarmallineni@gmail.com>
2026-07-04 12:17:24 +0530

    Update VPC module source repository

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "git::https://github.com/daws-90s/terraform-aws-vpc.git?ref=main"
+    source = "git::https://github.com/akhilkumarmallineni-lgtm/terraform-aws-vpc.git?ref=main"
     project = var.project
     environment = var.environment
     is_peering_required = false
