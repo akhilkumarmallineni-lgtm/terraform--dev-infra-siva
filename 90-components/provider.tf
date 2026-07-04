a48cb208465d864430807a7d17c281f625ee81ea
akhilkumarmallineni-lgtm <akhilkumarmallineni@gmail.com>
2026-07-04 13:21:16 +0530

    Update provider.tf

--- 90-components/provider.tf
+++ 90-components/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   # remote state storage location
   backend "s3" {
-    bucket         = "remote-state-90s-dev"
+    bucket         = "akhil8074"
     key            = "roboshop-components.tfstate"
     region         = "us-east-1"
     encrypt        = true
